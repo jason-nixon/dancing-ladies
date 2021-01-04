7e54b86bd940e92378e45e4a7582b3730b9f0a72
jnixon0789 <jnixon0789@gmail.com>
2021-01-03 18:10:07 -0600

    Fixed issue with not plotting (waitkey or some other pause are required for the command to finish plotting).

--- src/dancing-ladies.py
+++ src/dancing-ladies.py
@@ -1,9 +1,7 @@
 import numpy as np
 import cv2 as cv
-import argparse
 
 # Specify the filepath of the video to read.  
-
 video_file = 'C:\\repos\\dancing-ladies\\asset\\video\\ladies.avi'
 
 # Generate the background subtraction object (KNN or MOG2).  
@@ -12,6 +10,7 @@ backSub = cv.createBackgroundSubtractorKNN(history = 2, dist2Threshold = 75, det
 # Start reading the target video.  
 capture = cv.VideoCapture(cv.samples.findFileOrKeep(video_file))
 
+# Quit is video could not be opened.  
 if not capture.isOpened:
     print('Error: Unable to open: ' + args.input)
     exit(0)
@@ -27,30 +26,30 @@ while True:
     print(index)
 
     # Read frame from video.  
-    _, frame = capture.read()
+    _, Frame = capture.read()
 
     # If no more frames, done.  
-    if frame is None:
+    if Frame is None:
         print('Warning: Frame is empty.')
         break
 
     # Convert color space RGB -> Gray
-    # frame = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+    Frame = cv.cvtColor(Frame, cv.COLOR_RGB2GRAY)
 
-    # # Apply opening morphological operation.  
-    # frame = cv.morphologyEx(frame, cv.MORPH_OPEN, np.ones((5,5),np.uint8))
+    # Apply opening morphological operation.  
+    Frame = cv.morphologyEx(Frame, cv.MORPH_OPEN, np.ones((5,5),np.uint8))
 
     # Convert color space Gray -> RGB
-    # frame = cv.cvtColor(frame, cv.COLOR_GRAY2RGB)
+    Frame = cv.cvtColor(Frame, cv.COLOR_GRAY2RGB)
 
     # Convert colorspace: RGB -> HSV
-    # frame = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
+    Frame = cv.cvtColor(Frame, cv.COLOR_RGB2HSV)
     
     # Calculate the forground mask.  
-    fgMask = backSub.apply(frame)
-
-
-    # cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    ForegroundMask = backSub.apply(Frame)
 
     # Show the foreground mask.  
-    cv.imshow(winname = 'ForegroundMask', mat = frame)
+    cv.imshow(winname = 'ForegroundMask', mat = ForegroundMask)
+
+    # Paused imshow for X miliseconds, imshow doesn't work otherwise (same as matlab).  
+    keyboard = cv.waitKey(30)
