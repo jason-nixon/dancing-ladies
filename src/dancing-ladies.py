19ca98dd547b9db50eb8d15b201eed7790ee1a66
Jason Nixon <Jnixon0789@gmail.com>
2021-02-08 22:47:11 -0600

    Added in colored overlay on top of real frame to better visualize changes in operations.

--- src/dancing-ladies.py
+++ src/dancing-ladies.py
@@ -3,10 +3,11 @@ import cv2 as cv
 import os
 from pathlib import Path
 import config
+import math
 
 bDisplayFrames = True
 
-bGenerateOutputVideo = True
+bGenerateOutputVideo = False
 
 # Variable naming guide:  lower camel case (dromedary case).
 # a_ : array
@@ -16,10 +17,9 @@ bGenerateOutputVideo = True
 # o_ : object
 # s_ : string
 
-
 # create a class that encapsulates and 'simply' parameterizs all the image processing
-# steps that are currently being used.  The class should hold a local instance of the 
-# frame, for speed.  
+# steps that are currently being used.  The class should hold a local instance of the
+# frame, for speed.
 
 # Specify the filepath of the video to read.
 sVideoFullFilePath = 'C:\\repos\\dancing-ladies\\data\\raw\\ladies.avi'
@@ -30,10 +30,11 @@ oBackgroundSubtractor = cv.createBackgroundSubtractorKNN(history = 2, dist2Thres
 # Start reading the target video.
 oInputVideo = cv.VideoCapture(cv.samples.findFileOrKeep(sVideoFullFilePath))
 
+
 # Video input aFrame rate, width, and height (W and H must be cast as integers).
-fInputVideoFramerate = oInputVideo.get(cv.CAP_PROP_FPS)
+fFrameRate = oInputVideo.get(cv.CAP_PROP_FPS)
 nFrameWidth = int(oInputVideo.get(cv.CAP_PROP_FRAME_WIDTH))
-nInputVideoFrameHeight = int(oInputVideo.get(cv.CAP_PROP_FRAME_HEIGHT))
+fFrameHeight = int(oInputVideo.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 # Quit is video could not be opened.
 if not oInputVideo.isOpened:
@@ -45,12 +46,10 @@ if not os.path.isdir('C:\\repos\\dancing-ladies\\data\\processed\\'):
 
 # Create video object to write to.
 if bGenerateOutputVideo:
-    oOutputVideo = cv.VideoWriter('C:\\repos\\dancing-ladies\\data\\processed\\output.avi', cv.VideoWriter_fourcc('M','J','P','G') , fInputVideoFramerate, (nFrameWidth,nInputVideoFrameHeight))
+    oOutputVideo = cv.VideoWriter('C:\\repos\\dancing-ladies\\data\\processed\\output.avi', cv.VideoWriter_fourcc('M','J','P','G') , fFrameRate, (nFrameWidth,fFrameHeight))
 
 # Initialize aFrame index var.
 nFrameIndex = 0
-
-
 
 while True:
 
@@ -61,7 +60,9 @@ while True:
     # Read aFrame from video.
     bGrabbed, aFrameOriginal = oInputVideo.read()
 
-    aFrame = aFrameOriginal
+    aFrameOriginal = aFrameOriginal[1:fFrameHeight, 1:640, :]
+
+    aFrame = np.copy(aFrameOriginal)
 
     # If no more frames, done.
     if aFrame is None or aFrameOriginal is None:
@@ -83,10 +84,10 @@ while True:
     aFrame = cv.cvtColor(aFrame, cv.COLOR_RGB2HSV)
 
     # Calculate the forground mask.
-    aFrame = oBackgroundSubtractor.apply(aFrame)
+    aMask = oBackgroundSubtractor.apply(aFrame)
 
     # # Find all connected entities.
-    # aEntityList, aLabeledImage, aEntityStats, aEntityCentroids = cv.connectedComponentsWithStats(image = aFrame, connectivity = 8)
+    # aEntityList, aLabeledImage, aEntityStats, aEntityCentroids = cv.connectedComponentsWithStats(image = aMask, connectivity = 8)
 
     # # The background is included as the 0th entry of the entity statistics, and should be removed.
     # aEntityStats = aEntityStats[1:, -1]
@@ -99,69 +100,68 @@ while True:
     #     if aEntityStats[i] > config.nMinEntitySize:
     #         aTempFrame[aLabeledImage == i + 1] = 255
 
-    # aFrame = aTempFrame
+    # aMask = aTempFrame
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((25, 3),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((25, 3),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE,kernel = np.ones((50, 5),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE,kernel = np.ones((50, 5),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE,kernel = np.ones((3, 50),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE,kernel = np.ones((3, 50),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((100, 3),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((100, 3),np.uint8))
 
-    # contours = cv.findContours(aFrame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # contours = cv.findContours(aMask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     # contours = contours[0] if len(contours) == 2 else contours[1]
 
     # for nIndex in contours:
-    #     cv.drawContours(aFrame, [nIndex], 0, (255,255,255), -1)
+    #     cv.drawContours(aMask, [nIndex], 0, (255,255,255), -1)
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((1, 200),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((1, 200),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((300, 1),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((300, 1),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((100, 5),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((100, 5),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((1, 200),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((1, 200),np.uint8))
 
-    aFrame = cv.morphologyEx(src = aFrame, op = cv.MORPH_CLOSE, kernel = np.ones((300,1),np.uint8))
+    aMask = cv.morphologyEx(src = aMask, op = cv.MORPH_CLOSE, kernel = np.ones((300,1),np.uint8))
 
-    # contours = cv.findContours(aFrame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # contours = cv.findContours(aMask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     # contours = contours[0] if len(contours) == 2 else contours[1]
 
     # for index in contours:
-    #     cv.drawContours(aFrame, [index], 0, (255,255,255), -1)
+    #     cv.drawContours(aMask, [index], 0, (255,255,255), -1)
 
 
-    # aFrame = cv.morphologyEx(aFrame, cv.MORPH_CLOSE, np.ones((100,5),np.uint8))
+    # aMask = cv.morphologyEx(aMask, cv.MORPH_CLOSE, np.ones((100,5),np.uint8))
 
-    # aFrame = cv.morphologyEx(aFrame, cv.MORPH_CLOSE, np.ones((1,50),np.uint8))
+    # aMask = cv.morphologyEx(aMask, cv.MORPH_CLOSE, np.ones((1,50),np.uint8))
 
 
 
     # for index2 in range(0,2,1):
 
     #     for index in range(0,1,1):
-    #         aFrame = cv.morphologyEx(aFrame, cv.MORPH_CLOSE, np.ones((7,7),np.uint8))
+    #         aMask = cv.morphologyEx(aMask, cv.MORPH_CLOSE, np.ones((7,7),np.uint8))
 
     #     for index in range(0,1,1):
-    #         aFrame = cv.morphologyEx(aFrame, cv.MORPH_CLOSE, np.ones((1,100),np.uint8))
+    #         aMask = cv.morphologyEx(aMask, cv.MORPH_CLOSE, np.ones((1,100),np.uint8))
 
     # for index in range(0,1,1):
-    #     aFrame = cv.morphologyEx(aFrame, cv.MORPH_CLOSE, np.ones((1,250),np.uint8))
+    #     aMask = cv.morphologyEx(aMask, cv.MORPH_CLOSE, np.ones((1,250),np.uint8))
 
-    # aFrame = cv.morphologyEx(aFrame, cv.MORPH_CLOSE, np.ones((100,1),np.uint8))
+    aFrameOverlay = np.copy(aFrameOriginal)
 
-    # segment bottom to waistline and do close operation (prevents arms from getting caught in the closing operation).
+    aFrameOverlay[np.where(aMask == 255)] = [0,255,255]
 
-    # aFrameOriginal[(aFrame>=1).all(-1)] = [0,255,0]
-
+    cv.addWeighted(aFrameOverlay, 0.3, aFrameOriginal, 0.7, 0, aFrameOverlay)
 
     if bDisplayFrames:
 
         # Show the foreground mask.
-        cv.imshow(winname = 'ForegroundMask', mat = aFrame)
+        cv.imshow(winname = 'ForegroundMask', mat = aFrameOverlay)
 
         # Paused imshow for X miliseconds, imshow doesn't work otherwise (same as MATLAB).
         _ = cv.waitKey(delay = 30)
@@ -169,6 +169,8 @@ while True:
     # Write to output video object.
     if bGenerateOutputVideo:
         oOutputVideo.write(aFrame)
+
+    
 
 # Close all open opencv windows.
 cv.destroyAllWindows()
